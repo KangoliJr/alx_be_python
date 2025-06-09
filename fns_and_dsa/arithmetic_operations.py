65db49986021df6a02615b54d79e944cb20c0272
KangoliJr <joshua.kang'oli@students.uonbi.ac.ke>
2025-06-09 13:08:34 +0300

    My week 5 work

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation (num1, num2, operation):
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+    else:
+        return("Invalid operation. Please choose from +, -, *, /.")
+    
+    return result
+values = perform_operation
+print(values)
---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,20 @@
+from datetime import datetime, date, timedelta
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formatted_date}")
+    
+def calculate_future_date():
+    try:
+        days_to_add = int(input("Enter the days to add to the current date: "))
+    except ValueError:
+        print("Invalid input. Please enter an integer for the number of days.")
+        return
+    current_date_obj = date.today()
+    future_date = current_date_obj + timedelta(days=days_to_add)
+    formatted_future_date = future_date.strftime("%Y-m-%d")
+    print(f"Future date: {formatted_future_date}")
+    
+if __name__ == "__main__":
+    display_current_datetime
+    calculate_future_date
