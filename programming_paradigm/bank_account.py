36644f08f228902620fc3d099ec71cfa2c0d125e
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-06-20 00:29:27 +0300

    corrections

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,24 +2,24 @@ class BankAccount:
     def __init__(self,initial_balance=0):
         #starts the bank account at zero
         if not isinstance (initial_balance, (int, float)) or initial_balance <0:
-            raise ValueError("Initial must be zero")
+            raise ValueError("Initial balance must be a non-negative number.")
         self.account_balance = initial_balance
         
     def deposit(self, amount):
         if not isinstance(amount, (int, float)) or amount <=0:
-            raise ValueError("deposit amount thats greater than zero")
+            raise ValueError("Deposit amount must be a positive number.")
         self.account_balance += amount
         print(f"Deposited: ${amount:.1f}")
         
     def withdraw(self, amount):
         if not isinstance(amount, (int, float)) or amount <=0:
-            raise ValueError("Positive number")
+            raise ValueError("Withdrawal amount must be a positive number.")
         if self.account_balance >= amount:
            self.account_balance -= amount
-           print(f"Withdraw: ${amount:.2f}")
+           print(f"Withdrew: ${amount:.2f}")
            return True
         else:
-           print("Insufficient funds")
+           print("Insufficient funds.")
            return False
        
     def display_balance(self):
